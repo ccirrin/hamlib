d12f3d148b51fc9269e5fc45215966aa8e172e66
Lucas <lucashahn@comcast.net>
2020-04-15 13:35:12 -0400

    Added Local MetaData Creation

--- client.py
+++ client.py
@@ -1,6 +1,18 @@
-# checkMetadata()
+import os
+
+def checkMetadata():
     # get the metadata from firebase and check if there are any new files
+    #fmeta = firebase()
+    # for line in fmeta:
+    #     if "Files:" in line:
+    #         break
+    metadata = open('metadata','r')
+    for line in metadata:
+        if "Files:" in line:
+            break
+
     # if file match add to the downloads array
+
     # if a file is not found call requestFile from a random user in lists
 # requestFile()
     # choose a random peer thats online and try to download a file from them
--- init.py
+++ init.py
@@ -3,3 +3,18 @@
 # if there are files missing in download call peers to get files
 # call upload functions if there are files in upload folder
 # Create a list of files in upload and download folder
+files = []
+ips = []
+import client
+import os
+if __name__ == "__main__":
+    os.chdir("Downloads")
+    metadata = open("Metadata",'w')
+    metadata.write("IPs:\nFiles:\n")
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            if "Metadata" in file:
+                continue
+            metadata.write(file+"\n")
+    client.checkMetadata()
+    
