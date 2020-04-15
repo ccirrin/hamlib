ae47573d31c2120b6c2ede387bf855ef1c249388
Lucas <lucashahn@comcast.net>
2020-04-15 13:41:19 -0400

    Added check metadata need firebase connection

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import os
-
+import init
 def checkMetadata():
     # get the metadata from firebase and check if there are any new files
     #fmeta = firebase()
@@ -8,12 +8,19 @@ def checkMetadata():
     #         break
     metadata = open('metadata','r')
     for line in metadata:
+        init.ips.append(line.rstrip("\n"))
         if "Files:" in line:
             break
+    for line in metadata:
+        templine = line.rstrip("\n")
+        # if file match do nothing
+        if templine in init.files:
+            continue
+        # if a file is not found call requestFile from a random user in IP list
+        else
+            requestFile(templine)
 
-    # if file match add to the downloads array
 
-    # if a file is not found call requestFile from a random user in lists
 # requestFile()
     # choose a random peer thats online and try to download a file from them
     # switch after 20 seconds
