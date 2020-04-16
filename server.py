c370aeb2801fe5b037a87ef26e83dc657313e6eb
Lucas <lucashahn@comcast.net>
2020-04-16 10:59:34 -0400

    Added handeling of sending a file from a sever

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
-# sendFile()
+import socket
+import os
+
+def sendFile(file,sock):
     # Handle sending a file from downloads to a requester
+    http = "HTTP/1.1 200 OK\r\n" +"Content-Length: " + str(os.stat(file).st_size)+ " \r\n\r\n"
+    http = bytes(http, encoding='utf8')
+    sock.sendall(http)
+    f = open(file,"rb")
+    http= f.read()
+    sock.sendall(http)
+    f.close()
+
 
 # listen()
     # create a socket that handles listening for connections from other peers
