153c1bc6518ddfbfab953b5bef18d837f18c2fb2
Lucas <lucashahn@comcast.net>
2020-04-16 10:58:59 -0400

    CLient requesting files

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
-import os
 import init
+import random
+import socket
 def checkMetadata():
     # get the metadata from firebase and check if there are any new files
     #fmeta = firebase()
@@ -8,6 +9,8 @@ def checkMetadata():
     #         break
     metadata = open('metadata','r')
     for line in metadata:
+        if "IPs:" in line:
+            continue
         init.ips.append(line.rstrip("\n"))
         if "Files:" in line:
             break
@@ -19,9 +22,46 @@ def checkMetadata():
         # if a file is not found call requestFile from a random user in IP list
         else:
             requestFile(templine)
+    
+
 
 
 def requestFile(file):
     # choose a random peer thats online and try to download a file from them
-    # switch after 20 seconds
+    ip = init.ips[random.randint(0,len(init.ips)+1)]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(ip)
+    http="GET " + file + " HTTP/1.1\r\n\r\n"
+    sock.sendall(bytes(http, encoding = "utf-8"))
+    head = sock.recv(1024)
+    head = str(head)
+    #verify request       
+    while "404" in head:
+            sock.close()
+            print("Error 404: File "+ file+" not found. Trying different IP.")
+            ip = init.ips[random.randint(0,len(init.ips)+1)]
+            sock.connect(ip)
+            sock.sendall(bytes(http, encoding = "utf-8"))
+            head = sock.recv(1024)
+            head = str(head)
+    #find file length
+    length = head[head.index("Content-Length"):]
+    space = 0
+    size = ""
+    for x in length:
+            if space:
+                size=size + x
+            if x == " ":
+                space+=1
+                if space == 2:
+                    break
+    size= int(size)
+    data =bytes()
+    #receive messages until entire file arrives
+    while size > 0:
+        data += sock.recv(1024)
+        size -= 1024
+    fd = open(file,"wb")
+    fd.write(data)
+    fd.close()
     return
