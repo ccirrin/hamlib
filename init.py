973e9936c2f39b1e9d3dff574c465042c46ed98b
Carlo Cirrincione <ccirrin@gmail.com>
2020-04-22 12:49:44 -0500

    changes to fit new scheme

--- init.py
+++ init.py
@@ -1,29 +1,45 @@
-# Handle the grabing of initial metadata
-    # Call check metadata
-# call upload functions if there are files in upload folder
-# Create a list of files in upload and download folder
-files = []
-ips = []
-upfiles= []
 import client
 import uploads
 import os
 import threading
 import sys
 import server
-def init():
-    os.chdir("Downloads")
-    for root, dirs, fil in os.walk(os.getcwd()):
-        for file in fil:
-            if "Metadata" in file:
-                continue
-            files.append(file)
-    client.checkMetadata()
-    os.chdir("..\\Uploads")
-    for root, dirs, fil in os.walk(os.getcwd()):
-        for file in fil:
-            upfiles.append(file)
-    uploads.massUpload()
-    threading.Thread(target = server.peerListening, args = (sys.argv[1]).start())
+import fire
+import socket
 
+# Handle the grabing of initial metadata
+    # Call check metadata
+# call upload functions if there are files in upload folder
+# Create a list of files in upload and download folder
+
+files = []
+ips = []
+upfiles= []
+
+def init():
+    # Get ip address of host
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+
+    # Prompt user for channel they would like to join
+    user = fire.promptuser(ip)
+
+    # Check what files current user has and inform firebase
+    os.chdir("channel" + user.channel)
+    for root, dirs, fil in os.walk(os.getcwd()):
+        for f in fil:
+            fire.addfile(user, f)
+    
+    # Spin off server thread
+    # threading.Thread(target = server.peerListening, args = (sys.argv[1]).start())
+
+    # Spin off client thread
+    # cheeser
+
+    print("Type 'q' to quit!")
+    a = input()
+    while a != "q":
+        a = input()
+           
+if  __name__ == "__main__": init()
 
